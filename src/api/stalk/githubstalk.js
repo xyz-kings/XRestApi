644926d7f6c5d10212cc19f0ad22b93a991ce553
Xyz-King's <xyzkings22@gmail.com>
2025-12-19 11:29:36 +0800

    Auto upload via xyzup.py

---
+++ src/api/stalk/githubstalk.js
@@ -0,0 +1,47 @@
+const axios = require("axios")
+
+async function githubstalk(user) {
+    return new Promise((resolve, reject) => {
+        axios.get('https://api.github.com/users/'+user)
+        .then(({ data }) => {
+            let hasil = {
+                username: data.login,
+                nickname: data.name,
+                bio: data.bio,
+                id: data.id,
+                nodeId: data.node_id,
+                profile_pic: data.avatar_url,
+                url: data.html_url,
+                type: data.type,
+                admin: data.site_admin,
+                company: data.company,
+                blog: data.blog,
+                location: data.location,
+                email: data.email,
+                public_repo: data.public_repos,
+                public_gists: data.public_gists,
+                followers: data.followers,
+                following: data.following,
+                ceated_at: data.created_at,
+                updated_at: data.updated_at
+            }
+            resolve(hasil)
+        })
+    })
+}
+
+module.exports = function(app) {
+app.get('/stalk/github', async (req, res) => {
+       const { user } = req.query
+        try {
+            const results = await githubstalk(user);  
+            res.status(200).json({
+                status: true,
+                result: results
+            });
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+});
+
+}
