699db4aadf64bbb088b0c2fcbad855375c35d57e
Xyz-King's <xyzkings22@gmail.com>
2025-12-19 11:29:41 +0800

    Auto upload via xyzup.py

---
+++ src/api/stalk/npmstalk.js
@@ -0,0 +1,36 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+ 
+async function npmstalk(packageName) {
+  let stalk = await axios.get("https://registry.npmjs.org/"+packageName)
+  let versions = stalk.data.versions
+  let allver = Object.keys(versions)
+  let verLatest = allver[allver.length-1]
+  let verPublish = allver[0]
+  let packageLatest = versions[verLatest]
+  return {
+    name: packageName,
+    versionLatest: verLatest,
+    versionPublish: verPublish,
+    versionUpdate: allver.length,
+    latestDependencies: Object.keys(packageLatest.dependencies).length,
+    publishDependencies: Object.keys(versions[verPublish].dependencies).length,
+    publishTime: stalk.data.time.created,
+    latestPublishTime: stalk.data.time[verLatest]
+  }
+}
+
+module.exports = function (app) {
+app.get('/stalk/npm', async (req, res) => {
+       const { q } = req.query
+        try {
+            const results = await npmstalk(q);  
+            res.status(200).json({
+                status: true,
+                result: results
+            });
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+});
+}
