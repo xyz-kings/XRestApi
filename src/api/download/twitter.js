1f3c66b90fb75c57dc3e5f184c2d67ca8ab4062e
Xyz-King's <xyzkings22@gmail.com>
2025-12-19 11:30:23 +0800

    Auto upload via xyzup.py

---
+++ src/api/download/twitter.js
@@ -0,0 +1,78 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+async function downloadFromTwitter(id) {
+    try {
+        const url = 'https://ssstwitter.com';
+        const response = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36'
+            }
+        });
+
+        const $ = cheerio.load(response.data);
+        const form = $('form.pure-form.pure-g.hide-after-request');
+        const includeVals = form.attr('include-vals');
+        const ttMatch = includeVals.match(/tt:'([^']+)'/);
+        const tsMatch = includeVals.match(/ts:(\d+)/);
+
+        if (!ttMatch || !tsMatch) throw new Error('Cannot find tt or ts values.');
+
+        const tt = ttMatch[1];
+        const ts = tsMatch[1];
+
+        const postData = new URLSearchParams({
+            tt: tt,
+            ts: ts,
+            source: 'form',
+            id: id,
+            locale: 'en'
+        });
+
+        const postResponse = await axios.post(url, postData.toString(), {
+            headers: {
+                'HX-Request': 'true',
+                'HX-Target': 'target',
+                'HX-Current-URL': 'https://ssstwitter.com/en',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36',
+                'Referer': 'https://ssstwitter.com/result_normal'
+            }
+        });
+
+        const $result = cheerio.load(postResponse.data);
+        const downloads = [];
+        $result('.result_overlay a.download_link').each((i, element) => {
+            const text = $(element).text().trim();
+            const url = $(element).attr('href');
+            if (url) {
+                downloads.push({ text, url });
+            }
+        });
+
+        const data = {
+            title: $result('.result_overlay h2').text().trim(),
+            downloads: downloads
+        };
+
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+module.exports = function (app) {
+app.get('/download/twitter', async (req, res) => {
+       const { url } = req.query
+        try {
+            const results = await downloadFromTwitter(url);
+            res.status(200).json({
+                status: true,
+                result: results.downloads
+            });
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+});
+}
